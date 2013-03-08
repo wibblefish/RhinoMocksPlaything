3ff4afb0b652d4de4cf9fe09791e9f47f0c6562d
Stephen Newman <github@stephennewman.co.uk>
2013-03-08 17:54:51 +0000

    Adding a Person class
    and it's tests

--- RhinoMocksPlaything.Tests/PersonTest.cs
+++ RhinoMocksPlaything.Tests/PersonTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHamcrest.Core;
+
+using Assert = RhinoMocksPlaything.Tests.NHamcrestHelpers.Assert;
 
 namespace RhinoMocksPlaything.Tests
 {
@@ -8,13 +11,57 @@ namespace RhinoMocksPlaything.Tests
         //// ----------------------------------------------------------------------------------------------------------
 		 
         [TestMethod]
-        public void DefaultConstructor_Call_ExpectCorrectPerson()
+        public void DefaultConstructor_Call_ExpectIdOfZero()
         {
             // Arrange
 
             // Act
+            var person = new Person();
 
             // Assert
+            Assert.That(person.Id, Is.EqualTo(0));
+        }
+
+        //// ----------------------------------------------------------------------------------------------------------
+
+        [TestMethod]
+        public void DefaultConstructor_Call_ExpectNullFirstName()
+        {
+            // Arrange
+
+            // Act
+            var person = new Person();
+
+            // Assert
+            Assert.That(person.FirstName, Is.Null());
+        }
+
+        //// ----------------------------------------------------------------------------------------------------------
+
+        [TestMethod]
+        public void DefaultConstructor_Call_ExpectNullLastName()
+        {
+            // Arrange
+
+            // Act
+            var person = new Person();
+
+            // Assert
+            Assert.That(person.LastName, Is.Null());
+        }
+
+        //// ----------------------------------------------------------------------------------------------------------
+
+        [TestMethod]
+        public void DefaultConstructor_Call_ExpectNullFullName()
+        {
+            // Arrange
+
+            // Act
+            var person = new Person();
+
+            // Assert
+            Assert.That(person.FullName, Is.Null());
         }
 
         //// ----------------------------------------------------------------------------------------------------------
